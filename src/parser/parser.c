27d3b87842e51a2c9b3413e96ceed584d3736c6e
Jeremy FLEURY <jfleury@e2r7p16.42.fr>
2019-04-03 14:50:28 +0200

    Resolution des problemes de print, dans l'ordre

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: jfleury <jfleury@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/27 20:19:32 by jfleury           #+#    #+#             */
-/*   Updated: 2019/04/03 14:39:36 by jfleury          ###   ########.fr       */
+/*   Updated: 2019/04/03 14:49:42 by jfleury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,31 +27,31 @@ void	parser(t_lem *lem, t_room **room)
 	while ((get_next_line(0, &line)) == 1 && check == 1)
 	{
 		check = 0;
+		ft_printf("%s\n", line);
 		if (ft_comment(line) == 1)
 		{
-			ft_printf("ft_comment GNL N_%d / Check = 1\n", i);
+			ft_printf("GNL N_%d / ft_comment\n\n", i);
 			check = 1;
 		}
 		if (flag == 0 && ft_lem(line, lem, &flag) == 1)
 		{
-			ft_printf("ft_comment GNL N_%d / Check = 1\n", i);
+			ft_printf("GNL N_%d / ft_lem\n\n", i);
 			check = 1;
 		}
-/*		if (ft_command(line, lem, room) == 1)
+		if (ft_command(line, lem, room) == 1)
 		{
-			ft_printf("ft_comment GNL N_%d / Check = 1\n", i);
+			ft_printf("GNL N_%d / ft_command\n\n", i);
 			check = 1;
-		}*/
+		}
 		if (ft_room(line, room) == 1 && flag == 1)
 		{
-			ft_printf("ft_room GNL N_%d / Check = 1\n", i);
+			ft_printf("GNL N_%d ft_room\n\n", i);
 			check = 1;
 		}
 		/*
 		if (ft_path(line) == 1)
 			flag = 2;
 		*/
-		ft_printf("%s\n", line);
 		free(line);
 		i++;
 	}
